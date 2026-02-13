e259bce2b6b56dc76e2ea6218af60896d2225cb9
unknown <iskandar.django.work@gmail.com>
2026-02-13 14:50:12 +0500

    Middlewares: add centralized validation handling

---
+++ validation/validationMiddleware.js
@@ -0,0 +1,56 @@
+const { z } = require("zod");
+
+const registerSchema = z.object({
+  username: z
+    .string({ message: "Incorrect username type" })
+    .min(3, { message: "Username must be at least 3 characters long" })
+    .max(64, { message: "Username must be at max 64 characters long" }),
+  userEmail: z.email({ message: "Invalid email address" }),
+  password: z
+    .string({ message: "Incorrect password type" })
+    .min(8, { message: "Password must be at least 8 characters long" }),
+});
+
+const loginSchema = z.object({
+  userEmail: z.email({ message: "Invalid email address" }),
+  password: z
+    .string({ message: "Incorrect password type" })
+    .min(8, { message: "Password must be at least 8 characters long" }),
+});
+
+const updateSchema = z.object({
+  username: z
+    .string({ message: "Incorrect username type" })
+    .min(3, { message: "Username must be at least 3 characters long" })
+    .max(64, { message: "Username must be at max 64 characters long" }),
+});
+
+module.exports.registerValidation = (req, res, next) => {
+  try {
+    const validatedData = registerSchema.parse(req.body);
+    req.body = validatedData;
+    next();
+  } catch (e) {
+    next(e);
+  }
+};
+
+module.exports.loginValidation = (req, res, next) => {
+  try {
+    const validatedData = loginSchema.parse(req.body);
+    req.body = validatedData;
+    next();
+  } catch (e) {
+    next(e);
+  }
+};
+
+module.exports.updateValidation = (req, res, next) => {
+  try {
+    const validatedData = updateSchema.parse(req.body);
+    req.body = validatedData;
+    next();
+  } catch (e) {
+    next(e);
+  }
+};
